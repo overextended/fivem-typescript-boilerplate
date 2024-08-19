f42b26059defffa91cf5a14ff63d2a0de0a02542
Linden <65407488+thelindat@users.noreply.github.com>
2024-08-19 18:16:56 +1000

    feat(common/locale): type-safe locale function

---
+++ src/common/locale.ts
@@ -0,0 +1,14 @@
+import { locale, FlattenObjectKeys } from '@overextended/ox_lib';
+
+type RawLocales = FlattenObjectKeys<typeof import('../../locales/en.json')>;
+
+/**
+ *
+ */
+function Locale<T extends RawLocales>(str: T, ...args: any[]): string;
+function Locale<T extends string>(str: T, ...args: any[]): string | unknown;
+function Locale<T extends string>(str: T, ...args: any[]) {
+  return locale(str, ...args);
+}
+
+export default Locale;
