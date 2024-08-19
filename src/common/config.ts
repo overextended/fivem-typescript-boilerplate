f36b17b3849dc5bd148b3542b7335679b67db91e
Linden <65407488+thelindat@users.noreply.github.com>
2024-08-19 18:24:14 +1000

    refactor: add root path

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,4 +1,4 @@
-import type StaticConfig from '../../static/config.json';
+import type StaticConfig from '~/static/config.json';
 import { LoadJsonFile } from 'utils';
 
 export default LoadJsonFile<typeof StaticConfig>('static/config.json');
