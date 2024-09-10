0a4332e1b89ce480125dca94bc5774eac4c6ff69
Linden <65407488+thelindat@users.noreply.github.com>
2024-09-10 19:45:20 +1000

    refactor(scripts/build): use fx-utils

--- scripts/build.js
+++ scripts/build.js
@@ -1,77 +1,46 @@
 //@ts-check
 
-import esbuild from 'esbuild';
-import { readFile, writeFile } from 'fs/promises';
-import { exists, exec, getFiles, getPackage } from './utils.js';
+import { exists, exec, getFiles } from './utils.js';
+import { createBuilder, createFxmanifest } from '@overextended/fx-utils';
 
-/** @type {import('esbuild').BuildOptions} */
-const server = {
-  platform: 'node',
-  target: ['node16'],
-  format: 'cjs',
-};
-
-/** @type {import('esbuild').BuildOptions} */
-const client = {
-  platform: 'browser',
-  target: ['es2021'],
-  format: 'iife',
-};
-
-const pkg = await getPackage();
-let fxmanifest = `name '${pkg.name}'
-author '${pkg.author}'
-version '${pkg.version}'
-description '${pkg.description}'
-
-${await readFile('./src/fxmanifest.lua', 'utf8')}\n`;
-
-const environments = [];
-const production = process.argv.includes('--mode=production');
+const watch = process.argv.includes('--watch');
 const web = await exists('./web');
 
-if (web) fxmanifest += `ui_page 'dist/web/index.html'\n`;
-if (await exists('./src/client')) environments.push('client');
-if (await exists('./src/server')) environments.push('server');
+createBuilder(
+  watch,
+  {
+    dropLabels: !watch ? ['DEV'] : undefined,
+  },
+  [
+    {
+      name: 'server',
+      options: {
+        platform: 'node',
+        target: ['node16'],
+        format: 'cjs',
+      },
+    },
+    {
+      name: 'client',
+      options: {
+        platform: 'browser',
+        target: ['es2021'],
+        format: 'iife',
+      },
+    },
+  ],
+  async (outfiles) => {
+    const files = await getFiles('dist/web', 'static', 'locales');
+    await createFxmanifest({
+      client_scripts: [outfiles.client],
+      server_scripts: [outfiles.server],
+      files: ['lib/init.lua', 'lib/client/**.lua', 'locales/*.json', 'common/data/*.json', ...files],
+      dependencies: ['/server:7290', '/onesync'],
+      metadata: {
+        ui_page: 'dist/web/index.html',
+      },
+    });
+  }
+);
 
-const buildCmd = production ? esbuild.build : esbuild.context;
-
-for (const context of environments) {
-  fxmanifest += `${context}_script 'dist/${context}.js'\n`;
-
-  await buildCmd({
-    bundle: true,
-    entryPoints: [`./src/${context}/index.ts`],
-    outfile: `dist/${context}.js`,
-    keepNames: true,
-    dropLabels: production ? ['DEV'] : undefined,
-    legalComments: 'inline',
-    treeShaking: true,
-    plugins: production
-      ? undefined
-      : [
-          {
-            name: 'rebuild',
-            setup(build) {
-              build.onEnd((result) => console.log(`built ${context}.js with ${result.errors.length} errors`));
-            },
-          },
-        ],
-    ...(context === 'client' ? client : server),
-  })
-    .then((build) => {
-      if (production) return console.log(`Successfully built ${context}`);
-
-      build.watch();
-    })
-    .catch(() => process.exit(1));
-}
-
-if (web) await exec(`cd ./web && vite ${production ? 'build' : 'build --watch'}`);
-
-const files = await getFiles('dist/web', 'static', 'locales');
-
-fxmanifest += `\nfiles {\n\t'${files.filter((file) => !file.includes('/server')).join("',\n\t'")}',\n}`;
-
-writeFile('.yarn.installed', new Date().toISOString());
-writeFile('fxmanifest.lua', fxmanifest);
+if (web) await exec(`cd ./web && vite build ${watch ? '--watch' : ''}`);
